86442c6c5eeba9166da86d559b67900a10a05866
Ethan Phuong <ephuong@gmail.com>
2021-10-28 02:07:45 -0700

    Add files via upload

---
+++ FinalProject/app/src/main/java/com/example/cookingbythebook/compositepackage/CompositeClasses.kt
@@ -0,0 +1,52 @@
+package com.example.cookingbythebook.compositepackage
+
+import com.example.cookingbythebook.iteratorpackage.CategoryIterator
+import com.example.cookingbythebook.iteratorpackage.CookbookIterator
+import com.example.cookingbythebook.iteratorpackage.NullIterator
+
+abstract class Page(protected open var title: String) {
+    fun returnTitle(): String { return title }
+    fun changeTitle(__title: String) { title = __title }
+    abstract fun createIterator(): CookbookIterator
+}
+
+class Recipe(override var title: String) : Page(title) {
+    private var tags = ArrayList<String>()
+    private var ingredients = ArrayList<String>()
+    private var instructions = ArrayList<String>()
+
+    fun addTag(tag:String) { tags.add(tag) }
+    fun removeTag(tag:String) { tags.remove(tag) }
+    fun returnTag(loc: Int): String { return tags[loc] }
+    fun returnTagsCount(): Int { return tags.count() }
+
+    fun addIngredient(ingredient:String) { ingredients.add(ingredient) }
+    fun removeIngredient(ingredient:String) { ingredients.remove(ingredient) }
+    fun returnIngredient(loc: Int): String { return ingredients[loc] }
+    fun returnIngredientsCount(): Int { return ingredients.count() }
+
+    fun addInstruction(instruction:String) { instructions.add(instruction) }
+    fun removeInstruction(instruction:String) { instructions.remove(instruction) }
+    fun returnInstruction(loc: Int): String { return instructions[loc] }
+    fun returnInstructionsCount(): Int { return instructions.count() }
+
+    override fun createIterator(): CookbookIterator { return NullIterator(this) }
+}
+
+class Category(override var title: String) : Page(title) {
+    private var pages = ArrayList<Page>()
+
+    fun addPage(__page:Page) { pages.add(__page) }
+    fun removePage(__page:Page) { pages.remove(__page) }
+    fun returnPage(loc: Int): Page { return pages[loc] }
+    fun returnPagesCount(): Int { return pages.count() }
+  
+    override fun createIterator(): Iterator {
+        return if(returnPagesCount() > 0) {
+            CategoryIterator(this)
+        }
+        else {
+            NullIterator(this)
+        }
+    }
+}
---
+++ FinalProject/app/src/main/java/com/example/cookingbythebook/iteratorpackage/IteratorClasses.kt
@@ -0,0 +1,117 @@
+package com.example.cookingbythebook.iteratorpackage
+
+import com.example.cookingbythebook.compositepackage.Category
+
+import com.example.cookingbythebook.compositepackage.Page
+import java.util.*
+
+interface CookbookIterator {
+    fun getNext()
+    fun isDone(): Boolean
+    fun first()
+    fun current(): Page?
+}
+
+
+interface PreorderIteratorInterface : CookbookIterator {
+    var iterators: ArrayDeque<CookbookIterator>
+}
+
+class CategoryIterator(var arr: Category) : CookbookIterator{
+    private var atEnd: Boolean = false
+    private var index: Int = 0
+    override fun getNext() {
+        when {
+            index < arr.returnPagesCount() - 1 -> {
+                index++
+            }
+            index == arr.returnPagesCount() - 1 -> {
+                atEnd = true
+            }
+            else -> {
+                atEnd = true
+            }
+        }
+    }
+
+    override fun isDone(): Boolean {
+        if (atEnd) {
+            return true
+        }
+        return false
+    }
+
+    override fun first() {
+        index = 0
+    }
+
+    override fun current(): Page? {
+        return arr.returnPage(index)
+    }
+}
+
+class PreorderIterator(var titlePage: Page?) : CookbookIterator, PreorderIteratorInterface {
+
+    override var iterators = ArrayDeque<CookbookIterator>()
+
+    override fun first(){
+        while(!iterators.isEmpty())
+        {
+            iterators.pop()
+        }
+        if(titlePage != null) {
+            val rootIterator: CookbookIterator? = titlePage?.createIterator()
+            rootIterator?.first()
+            iterators.push(rootIterator)
+        }
+    }
+
+    override fun getNext() {
+        val topIterator: CookbookIterator? = iterators.first().current()?.createIterator()
+        topIterator?.first()
+        iterators.push(topIterator)
+        while(!iterators.isEmpty() && iterators.first().isDone())
+        {
+            iterators.pop()
+            if(!iterators.isEmpty())
+            {
+                iterators.first().getNext()
+            }
+        }
+    }
+
+    override fun isDone(): Boolean {
+        if (iterators.isEmpty())
+        {
+            return true
+        }
+        return false
+    }
+
+    override fun current(): Page? {
+        return iterators.first().current()
+    }
+}
+
+class NullIterator(var titlePage: Page?) : CookbookIterator
+{
+    override fun current(): Page? {
+        return null
+    }
+
+    override fun getNext() {
+    }
+
+    override fun isDone(): Boolean {
+        return true
+    }
+
+    override fun first() {
+    }
+
+}
+
+
+
+
+
